d866fb23cecdb6de4e7df96f5be7ca57135bfe2e
Burak Kosova <burak.kosova25@gmail.com>
2021-07-07 10:53:40 +0300

    while ve foreach eklendi

---
+++ Pratikler/whileForEach/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace whileForEach
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int sayi = int.Parse(Console.ReadLine());
+      int sayac = 1;
+      int toplam = 0;
+      while (sayac <= sayi)
+      {
+        toplam += sayac;
+        sayac++;
+      }
+      Console.WriteLine(toplam / sayi);
+
+      char c = 'a';
+      while (c < 'z')
+      {
+        Console.WriteLine(c);
+        c++;
+      }
+
+      string[] arabalar = { "BMW", "Porsche", "Ford", "Ferrari", "Nissan" };
+      foreach (string araba in arabalar)
+      {
+        Console.WriteLine(araba);
+      }
+    }
+  }
+}

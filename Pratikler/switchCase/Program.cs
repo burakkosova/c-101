0aa44478158b605a4f4659778fc7bf2cb4763ac1
Burak Kosova <burak.kosova25@gmail.com>
2021-07-07 10:22:22 +0300

    switch case kodları eklendi

---
+++ Pratikler/switchCase/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace switchCase
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int month = DateTime.Now.Month;
+
+      switch (month)
+      {
+        case 1:
+          System.Console.WriteLine("Ocak ayındasınız!");
+          break;
+        case 2:
+          System.Console.WriteLine("Şubat ayındasınız!");
+          break;
+        case 3:
+          System.Console.WriteLine("Mart ayındasınız!");
+          break;
+        case 4:
+          System.Console.WriteLine("Nisan ayındasınız!");
+          break;
+        case 5:
+          System.Console.WriteLine("Mayıs ayındasınız!");
+          break;
+        case 6:
+          System.Console.WriteLine("Haziran ayındasınız!");
+          break;
+        case 7:
+          System.Console.WriteLine("Temmuz ayındasınız!");
+          break;
+        default:
+          System.Console.WriteLine("yanlış deger!");
+          break;
+      }
+    }
+  }
+}

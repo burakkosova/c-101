091ce8ff8dfbc193f6a6dd4bcb21dc7df237dab6
Burak Kosova <burak.kosova25@gmail.com>
2021-07-07 09:12:22 +0300

    first commit

---
+++ Pratikler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pratikler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}

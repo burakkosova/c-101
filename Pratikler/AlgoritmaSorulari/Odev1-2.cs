aa9258bbe3da88f660e7e0d7ceafe1dd5df49c1a
Burak Kosova <burak.kosova25@gmail.com>
2021-07-18 17:58:43 +0300

    c# 101 Odev 1 2. kısım eklendi

---
+++ Pratikler/AlgoritmaSorulari/Odev1-2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PatikaC101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen 2 adet pozitif sayı giriniz");
+            Console.Write("1. Sayı: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("2. Sayı: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = new int[n];
+            Console.WriteLine(n + " tane pozitif sayı giriniz:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(i+1 + ". sayı: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.Write("Girdiğiniz sayılardan {0} sayısına tam bölünenler: ",m);
+
+            foreach (int i in numbers)
+            {
+                if(i%m == 0)
+                    Console.Write(i + " ");
+            }
+        }
+    }
+}

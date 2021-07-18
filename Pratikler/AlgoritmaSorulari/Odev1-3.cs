ebc3bda1eb661ec2ca36d2866bd193879d90cf5f
Burak Kosova <burak.kosova25@gmail.com>
2021-07-18 18:02:57 +0300

    c# 101 Odev 1 3. kısım eklendi

---
+++ Pratikler/AlgoritmaSorulari/Odev1-3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PatikaC101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir pozitif sayı giriniz: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            string[] words = new string[n];
+            Console.WriteLine(n + " tane kelime giriniz:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(i+1 + ". kelime: ");
+                words[i] =Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            Console.Write("Girdiğiniz sayılardan kelimeler(sondan başa doğru): ");
+
+            for (int i=words.Length - 1; i >= 0; i--)
+            {
+                    Console.Write(words[i] + " ");
+            }
+        }
+    }
+}

6bada43bc89586ae968d1523d5ed07b0db8e4797
Burak Kosova <burak.kosova25@gmail.com>
2021-07-18 18:06:34 +0300

    c# 101 Odev 1 4. kısım eklendi

---
+++ Pratikler/AlgoritmaSorulari/Odev1-4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PatikaC101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir cümle giriniz: ");
+            string sentence = Console.ReadLine();
+            Console.WriteLine();
+
+            string[] words = sentence.Split(" ");
+            Console.WriteLine("Girdiğiniz cümle {0} harf ve {1} kelimeden oluşmaktadır.",sentence.Length,words.Length);
+        }
+    }
+}

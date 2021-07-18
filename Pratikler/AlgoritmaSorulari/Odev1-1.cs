b585486d3439fba6370e0b9cfde04c0a526de4b1
Burak Kosova <burak.kosova25@gmail.com>
2021-07-18 17:54:12 +0300

    c# 101 Odev 1 1. kısım eklendi

---
+++ Pratikler/AlgoritmaSorulari/Odev1-1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PatikaC101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç tane sayı gireceksiniz? ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[n];
+            Console.WriteLine(n + " tane pozitif sayı giriniz:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(i+1 + ". sayı: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.Write("Girdiğiniz pozitif sayılar: ");
+
+            foreach (int i in numbers)
+            {
+                if(i%2 == 0)
+                    Console.Write(i + " ");
+            }
+        }
+    }
+}

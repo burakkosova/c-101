1d5b4622563e390309576397e124178ae7889561
Burak Kosova <burak.kosova25@gmail.com>
2022-03-16 09:40:09 +0300

    koleksiyonlar soru 3 eklendi

---
+++ Pratikler/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir cümle giriniz: ");
+            string sentence = Console.ReadLine();
+            ArrayList vowels = new ArrayList();
+
+            foreach (char letter in sentence)
+            {
+                if ("aeioöuüAEIİOÖUÜ".Contains(letter))
+                {
+                    vowels.Add(letter);
+                }
+            }
+
+            foreach (char item in vowels)
+            {
+                Console.Write(item+" ");
+            }
+        }
+    }
+}
